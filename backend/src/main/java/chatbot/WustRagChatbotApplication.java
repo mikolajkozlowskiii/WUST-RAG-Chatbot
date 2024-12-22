7180632697794f076c4ddb42f751f19b4fdcd7ef
mikolajkozlowskiii <mikolajkozlowskiii@gmail.com>
2024-12-23 00:46:52 +0100

    feat(chatbot): implement chatbot main loop and person identification flow

--- backend/src/main/java/chatbot/WustRagChatbotApplication.java
+++ backend/src/main/java/chatbot/WustRagChatbotApplication.java
@@ -3,31 +3,46 @@ package chatbot;
 import chatbot.llm.service.ChatbotService;
 import chatbot.speech.service.SpeechToTextService;
 import chatbot.speech.service.TextToSpeechService;
-import lombok.RequiredArgsConstructor;
+import chatbot.vision.CameraService;
+import chatbot.vision.FaceIdentifyService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.awt.image.BufferedImage;
+import java.util.concurrent.ExecutionException;
+
 @SpringBootApplication
 @Slf4j
 public class WustRagChatbotApplication {
 
-    private final ChatbotService chatbotService;
-    private final SpeechToTextService speechToTextService;
-    private final TextToSpeechService textToSpeechService;
+    private  ChatbotService chatbotService;
+    private  CameraService cameraService;
+    private  SpeechToTextService speechToTextService;
+    private  TextToSpeechService textToSpeechService;
+    private FaceIdentifyService faceIdentifyService;
 
-    public WustRagChatbotApplication(ChatbotService chatbotService, SpeechToTextService speechToTextService, TextToSpeechService textToSpeechService) {
+
+    public WustRagChatbotApplication(
+            ChatbotService chatbotService,
+            CameraService cameraService,
+            SpeechToTextService speechToTextService,
+            TextToSpeechService textToSpeechService,
+            FaceIdentifyService faceIdentifyService) {
+        this.cameraService = cameraService;
         this.chatbotService = chatbotService;
         this.speechToTextService = speechToTextService;
         this.textToSpeechService = textToSpeechService;
+        this.faceIdentifyService = faceIdentifyService;
     }
 
-    private static final String WELCOME_MESSAGE = "Cześć! Jestem wirtualnym asystentem dziekanatu " +
+    private static final String WELCOME_VOICE_MESSAGE = "Cześć! Jestem wirtualnym asystentem dziekanatu " +
             "wydziału informatyki i telekomunikacji na Politechnice Wrocławskiej! Jak mogę Ci pomóc?";
-    private static final String EXCEPTION_MESSAGE = "Bardzo mi przykro, ale wystąpił błąd. Proszę spróbuj ponownie.";
-
+    private static final String EXCEPTION_VOICE_MESSAGE = "Bardzo mi przykro, ale wystąpił błąd. Proszę spróbuj ponownie.";
+    private static final String TAKE_PHOTO_VOICE_MESSAGE = "Proszę spojrzeć w kamerę, aby zrobić zdjęcie przed rozpoczęciem rozmowy.";
+    private static final String RETAKE_PHOTO_VOICE_MESSAGE = "Nie udało się rozpoznać studenta wydziału Informatyki i Telekomunikacji.";
 
     public static void main(String[] args) {
         SpringApplication.run(WustRagChatbotApplication.class, args);
@@ -36,23 +51,54 @@ public class WustRagChatbotApplication {
     @Bean
     CommandLineRunner runChatbot() {
         return args -> {
-            log.info("Starting W4N Informatics and Telecommunication Faculty Chatbot...");
-            textToSpeechService.textToSpeech(WELCOME_MESSAGE);
-
-            while (true) {
-                log.info("Waiting for your input...");
-
-                try {
-                    final String recognizedSpokenText = speechToTextService.listenToUser();
-
-                    final String llmResponse = chatbotService.getResponse(recognizedSpokenText);
-
-                    textToSpeechService.textToSpeech(llmResponse);
-                } catch (Exception e) {
-                    log.error("Error occurred during speech processing", e);
-                    textToSpeechService.textToSpeech(EXCEPTION_MESSAGE);
+            log.info("start");
+            boolean isPersonIdentified = false;
+            try {
+                while (!isPersonIdentified){
+                    isPersonIdentified = identifyPerson();
                 }
+            } catch (Exception e) {
+                log.error("Error capturing photo", e);
+                textToSpeechService.textToSpeech("Nie udało się zrobić zdjęcia. Spróbuj ponownie.");
+                return;
             }
+
+            log.info("Starting W4N Informatics and Telecommunication Faculty Chatbot...");
+            textToSpeechService.textToSpeech(WELCOME_VOICE_MESSAGE);
+
+            startChatbotLoop();
         };
+    }
+
+    private boolean identifyPerson() throws Exception {
+        boolean isPersonIdentified;
+        textToSpeechService.textToSpeech(TAKE_PHOTO_VOICE_MESSAGE);
+        BufferedImage image = cameraService.capturePhoto();
+        log.info("Photo captured successfully.");
+        Thread.sleep(10000);
+        isPersonIdentified = faceIdentifyService.isFaceRecognized(image);
+        log.info("Is person identified " + isPersonIdentified);
+        Thread.sleep(10000);
+        if(isPersonIdentified){
+            textToSpeechService.textToSpeech(RETAKE_PHOTO_VOICE_MESSAGE);
+        }
+        return isPersonIdentified;
+    }
+
+    private void startChatbotLoop() throws InterruptedException, ExecutionException {
+        while (true) {
+            log.info("Waiting for your input...");
+
+            try {
+                final String recognizedSpokenText = speechToTextService.listenToUser();
+
+                final String llmResponse = chatbotService.getResponse(recognizedSpokenText);
+
+                textToSpeechService.textToSpeech(llmResponse);
+            } catch (Exception e) {
+                log.error("Error occurred during speech processing", e);
+                textToSpeechService.textToSpeech(EXCEPTION_VOICE_MESSAGE);
+            }
+        }
     }
 }
